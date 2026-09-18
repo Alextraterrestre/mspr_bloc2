784e9f40f4cfdf5d372bd8e6b805c86961e9e49d
alexis.carlier07 <alexis.carlier07@gmail.com>
2026-09-18 09:57:15 +0200

    modify vite.config port

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     proxy: {
       '/function':
       {
-        target: 'http://gateway.openfaas:8080',
+        target: 'http://gateway.openfaas:31112',
         changeOrigin: true
       }
     },
